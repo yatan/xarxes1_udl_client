2b3b0d19053e321571f49a596cd0893ded027413
yatan <yatan87@gmail.com>
2017-02-15 11:57:31 +0100

    lectura fitxer configuracio

--- client.c
+++ client.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <socket.h>
+#include <string.h>
+
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <sys/select.h>
+#include <sys/mman.h>
 
 
 /* Tipus paquet subscripcio */
@@ -47,32 +53,95 @@ struct client
 
 struct element
 {
-  char magnitud[3];
-  char ordinal[1];
-  char tipus[1];
+    char magnitud[3];
+    char ordinal[1];
+    char tipus[1];
 };
 struct element* elements;
 
 /*Estructura de dades TCP*/
 struct PDU_tcp
 {
-	unsigned char type;
-	char mac[7];
-	char aleatori[13];
-	char dispositiu[8];
-	char valor[7];
-	char info[80];
+    unsigned char type;
+    char mac[7];
+    char aleatori[13];
+    char dispositiu[8];
+    char valor[7];
+    char info[80];
 };
 
 int main(int argc, char **argv)
 {
-  FILE *f;
+    FILE *fp;
+    char line_buffer[BUFSIZ];
 
-  f = fopen(fitxer_conf,"r");
-  if(f == NULL)
-  {
-    printf("No es pot obrir el fitxer per registrar dades\n");
-  }
+    char identificador[8];
+    char situation[12];
+    char elements[7];
+    char MAC[12];
+    int local_tcp;
+    char server_ip[15];
+    int server_udp;
 
-  printf("Client\n");
+
+
+    fp = fopen(fitxer_conf,"r");
+    if(fp == NULL)
+    {
+        printf("No es pot obrir el fitxer per registrar dades\n");
+    }
+
+
+    /* Name */
+    fgets(line_buffer, sizeof(line_buffer), fp);
+    strtok(line_buffer, " ");
+    strxfrm(identificador,strtok(NULL, " "), 9);
+
+
+    /* Situation */
+    fgets(line_buffer, sizeof(line_buffer), fp);
+    strtok(line_buffer, " ");
+    strxfrm(situation,strtok(NULL, " "), 13);
+
+
+    /* Elements */
+    fgets(line_buffer, sizeof(line_buffer), fp);
+    strtok(line_buffer, " ");
+    strxfrm(elements,strtok(NULL, " "), 8);
+
+    /* MAC */
+    fgets(line_buffer, sizeof(line_buffer), fp);
+    strtok(line_buffer, " ");
+    strxfrm(MAC,strtok(NULL, " "), 13);
+
+    /* Local-TCP */
+    fgets(line_buffer, sizeof(line_buffer), fp);
+    strtok(line_buffer, " ");
+    local_tcp = atoi(strtok(NULL, " "));
+
+    /* Server IP */
+    fgets(line_buffer, sizeof(line_buffer), fp);
+    strtok(line_buffer, " ");
+    server_ip[strlen(server_ip) - 1] = '\0';
+    /* Al tenir direccions on no es posen tots els numeros, el valor es variable */
+    strxfrm(server_ip,strtok(NULL, " "), 16);
+
+
+    /* Server UDP port */
+    fgets(line_buffer, sizeof(line_buffer), fp);
+    strtok(line_buffer, " ");
+    server_udp = atoi(strtok(NULL, " "));
+
+
+    printf("Client %s\n", identificador);
+    printf("Situation %s\n", situation);
+    printf("Elements %s\n", elements);
+    printf("MAC %s\n", MAC);
+    printf("Local TCP Port: %i\n", local_tcp);
+    printf("Server ip: %s\n", server_ip);
+    printf("Server upd port: %i\n", server_udp);
+
+
+    fclose(fp);
+    return 0;
 }
